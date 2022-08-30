9b44b3b6e2d5f88c6078681f328173abb2f9998f
Kayla Alewine <kayla.r.alewine@gmail.com>
2022-08-30 15:31:10 -0400

    Create flex containers on therapist home

--- src/therapist/pages/therapistHome.js
+++ src/therapist/pages/therapistHome.js
@@ -4,16 +4,37 @@ import {
     Flex,
     Image,
     Heading,
+    Text, 
+    Button, 
+    VStack,
+    Avatar
   } from '@chakra-ui/react';
 
 function TherapistHome () {
     return (
-      <Box>
+      <Box width="80%" margin="auto">
         <Flex justify="start" alignItems="end">
-          <Image src="my-hep-logo.svg"></Image>
-          <Heading marginBottom="3rem">Welcome to My HEP</Heading>
+          <Image 
+            src="my-hep-logo.svg"
+            boxSize="8rem"></Image>
+          <Heading marginBottom="1.75rem" marginLeft="2rem">Welcome to My HEP</Heading>
         </Flex>
-       </Box>  
+        <Flex>
+          <VStack spacing="3.5rem" align='center' padding="2rem" marginTop="2rem">
+            <Text as='b'>Currently serving 45 patients</Text>
+            <Button colorScheme="twitter" variant="outline">My Patients</Button>
+            <Text as='b'>75 HEPs uploaded</Text>
+            <Button colorScheme="twitter" variant="outline">My HEPs</Button>
+          </VStack>
+          <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='2xl/>
+
+          
+          <VStack>
+
+          </VStack>
+        </Flex>
+      </Box>
+       
     );
   }
   
