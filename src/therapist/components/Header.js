7b195d79860d3137df4a16f421dd633f685fcc4c
Kayla Alewine <kayla.r.alewine@gmail.com>
2022-08-30 11:55:42 -0400

    Tune styling for header

--- src/therapist/components/Header.js
+++ src/therapist/components/Header.js
@@ -2,21 +2,24 @@ import React from "react";
 import {
   Box,
   Flex,
+  Spacer,
   Heading,
     Image
   } from '@chakra-ui/react';
 
 function Header () {
     return (
-      <Box >
-        <Flex justify="end" align="center" fontSize="xl">
-            <Image
-              boxSize='5rem'
-              src="my-hep-logo.svg" alt="dumbell inside house"/>
-            <Heading>My HEP</Heading>
+      <Box display="flex" justifyContent="end">
+          <Flex justify="end" alignItems="end" fontSize="xl" padding='1rem' minWidth="250px">
+              <Image
+                boxSize='5rem'
+                src="my-hep-logo.svg" alt="dumbell inside house"
+              />
+              <Spacer />
+              <Heading minWidth="130px">My HEP</Heading>
+          </Flex>
+        </Box>
       
-        </Flex>
-      </Box>
     );
   }
   
