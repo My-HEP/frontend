6cc22704af303b5b02a69ddf11cceb186d260e99
Kristen Coy <kristenwcoy@gmail.com>
2022-08-31 11:26:38 -0400

    Added responsive bottomNav component

---
+++ src/therapist/components/BottomNav.js
@@ -0,0 +1,41 @@
+import { Box, Button, Flex, HStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { IconHome, IconUsers, IconBarbell } from '@tabler/icons';
+import { ColorModeSwitcher } from '../../ColorModeSwitcher';
+
+const BottomNav = () => {
+  return (
+    <Box
+      as="nav"
+      position="fixed"
+      bg="#2C7A7B"
+      height="85px"
+      width="100%"
+      bottom="0"
+      display={{ base: 'block', md: 'none' }}
+    >
+      <Flex direction="row" bottom="0" height="100%" justify="center">
+        <HStack spacing="19" align="center">
+          <Link to="/therapisthome">
+            <Button variant="ghost">
+              <IconHome size={35} />
+            </Button>
+          </Link>
+          <Link to="/patients">
+            <Button variant="ghost">
+              <IconUsers size={35} />
+            </Button>
+          </Link>
+          <Link to="/HEPs">
+            <Button variant="ghost">
+              <IconBarbell size={35} />
+            </Button>
+          </Link>
+          <ColorModeSwitcher />
+        </HStack>
+      </Flex>
+    </Box>
+  );
+};
+
+export default BottomNav;
