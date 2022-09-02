eac8c6fb79f7ab6f7fcd4e440830c6c1386f220f
Kayla Alewine <kayla.alewine@bah.com>
2022-09-01 21:45:41 -0400

    Improve responsive design of therapist home

--- src/therapist/pages/TherapistHome.js
+++ src/therapist/pages/TherapistHome.js
@@ -25,19 +25,30 @@ function TherapistHome () {
         paddingTop="2rem"
         paddingBottom="100px"
       >
-        <Flex gap='5' marginBottom='2rem' align='center'>
+        <Flex 
+          gap='5' 
+          mb={['0rem', '0rem', '2rem']} 
+          align='center' 
+          justify={['center', 'center', 'start' ]}
+          minWidth={['300px', '300px', '500px']}
+        >
           <Icon as={logoIcon} boxSize="8rem"/>
           <Heading marginTop='1.65rem'>Welcome to My HEP</Heading>
         </Flex>
         <Flex 
           direction={['column', 'column', 'row']}
-          margin="2rem 0 2rem 0"
+          margin={['1rem 0', '1rem 0', '2rem 0']}
           align='center'
           justify='start'
         >
-          <VStack spacing={5}>
+          <VStack 
+            spacing={[3, 3, 5]}
+          >
             <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='2xl'/>
-            <VStack spacing={5} align='start'>
+            <VStack 
+              spacing={5} 
+             align='start'
+            >
               <Flex minWidth='175px' gap='2' justify='start'>
                 <Text as='b' fontSize='2xl'>Therapist Name</Text>
               </Flex>
@@ -55,7 +66,11 @@ function TherapistHome () {
               </HyperLink>
             </VStack>
           </VStack>
-          <VStack spacing={10} width='50%'>
+          <VStack 
+            spacing={[5, 5, 10]} 
+            width='50%'
+            mt={['3rem', '3rem', 0]}
+          >
             <Text as='b'>Currently serving 45 patients</Text>
             <Link to="/patients"><Button leftIcon={<IconUsers />} colorScheme="teal" variant="outline" size='lg' minWidth='12rem'>My Patients</Button></Link>
             <Text as='b'>75 HEPs uploaded</Text>
