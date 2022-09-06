074f9d485e88f25470df21adcd36e48f723717cf
Kayla Alewine <kayla.alewine@bah.com>
2022-09-06 11:46:32 -0400

    Update Edit Info Form

--- src/therapist/sharedComponents/EditInfoForm.js
+++ src/therapist/sharedComponents/EditInfoForm.js
@@ -11,118 +11,76 @@ import {
   EditableInput, 
   Avatar,
   Stack, 
-  VStack
+  VStack,
+  Flex
 } from "@chakra-ui/react";
 import { CheckIcon, CloseIcon } from "@chakra-ui/icons";
 import { IconPhone, IconMail,  IconLock, IconPencil, IconUser } from "@tabler/icons";
 
 const InfoForm = () => {
-  function EditableControls() {
-    const {
-      isEditing,
-      getSubmitButtonProps,
-      getCancelButtonProps,
-      getEditButtonProps,
-    } = useEditableControls()
-
-    return isEditing ? (
-      <ButtonGroup alignItems='center' justifyContent='center' size='sm'>
-        <IconButton colorScheme='teal' variant='ghost' aria-label='confirm changes' icon={<CheckIcon />} {...getSubmitButtonProps()} />
-        <IconButton colorScheme='teal' variant='ghost' aria-label='discard changes' icon={<CloseIcon />} {...getCancelButtonProps()} />
-      </ButtonGroup>
-    ) : (
-      
-        <IconButton size='xs' icon={<IconPencil />} aria-label='edit content' variant='ghost' colorScheme='teal' {...getEditButtonProps()} />
-      
-    )
-  }
-
+  
   return (
-    <Stack spacing={4}>
-    
-        <Editable spacing={[3, 3, 5]} align='center' marginLeft='1.5rem'>
-          <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='2xl'/>
-          <EditableControls justify='end'/> 
+    <VStack margin="1rem auto">
+      <Stack spacing={4} width='50%'>
+        <Editable spacing={[3, 3, 5]} marginLeft='1.5rem'>
+          <Flex justify='center' align='end'>
+            <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' size='2xl'/>
+            <IconButton size='xs' icon={<IconPencil />} aria-label='edit content' variant='ghost' colorScheme='teal' />
+          </Flex>
         </Editable>
-   
-
-      <Editable 
-        defaultValue='therapist name'
-        isPreviewFocusable={true}
-        display='flex'
-      >
-        <InputGroup minWidth='250px'>
-          <EditablePreview marginLeft='3rem' />
-          <InputLeftElement children={<IconUser />}/>
-          <Input type='text' as={EditableInput} /> 
-        </InputGroup>
-        {/* <EditableControls />  */}
-      </Editable>
-      <Editable 
-        defaultValue='primary number'
-        isPreviewFocusable={true}
-        display='flex'
-      >
-        <InputGroup minWidth='250px'>
-          <EditablePreview marginLeft='3rem' />
-          <InputLeftElement children={<IconPhone />}/>
-          <Input type='tel' as={EditableInput} /> 
-        </InputGroup>
-        {/* <EditableControls />  */}
-      </Editable>
-      <Editable 
-          defaultValue='email'
+        <Editable 
+          defaultValue='therapist name'
           isPreviewFocusable={true}
           display='flex'
         >
-          <InputGroup minWidth='250px'>
-            <EditablePreview marginLeft='3rem' />
+          <InputGroup >
+            <EditablePreview marginLeft='2.5rem' marginTop='5px' />
+            <InputLeftElement children={<IconUser />}/>
+            <Input type='text' as={EditableInput}/> 
+          </InputGroup>
+          {/* <EditableControls />  */}
+        </Editable>
+        <Editable 
+          defaultValue='primary number'
+          isPreviewFocusable={true}
+          display='flex'
+        >
+          <InputGroup>
+            <EditablePreview marginLeft='2.5rem' marginTop='5px'  />
+            <InputLeftElement children={<IconPhone />}/>
+            <Input type='tel' as={EditableInput} /> 
+          </InputGroup>
+          {/* <EditableControls />  */}
+        </Editable>
+        <Editable 
+            defaultValue='email'
+            isPreviewFocusable={true}
+            display='flex'
+        >
+          <InputGroup>
+            <EditablePreview marginLeft='2.5rem' marginTop='5px' />
             <InputLeftElement children={<IconMail />}/>
             <Input type='email' as={EditableInput} /> 
           </InputGroup>
           {/* <EditableControls />  */}
-        </Editable>
+          </Editable>
         <Editable 
           defaultValue='password'
           isPreviewFocusable={true}
           display='flex'
         >
-          <InputGroup minWidth='250px'>
-            <EditablePreview marginLeft='3rem' />
+          <InputGroup>
+            <EditablePreview marginLeft='2.5rem' marginTop='5px' />
             <InputLeftElement children={<IconLock />}/>
             <Input type='password' as={EditableInput}/> 
           </InputGroup>
           {/* <EditableControls />  */}
-        </Editable>
-          
-    </Stack>
+      </Editable>
+      </Stack>
+    </VStack>
   )
 }
   
-  // return (
-  //   <VStack>
-  //   <Editable defaultValue='Therapist Name'>
-  //             <EditablePreview />
-  //             <EditableInput />
-  //           </Editable>
-  //           <Text >Jane McTherapist</Text>
-  //           <Flex>
-  //             <Icon as={IconPhone} />
-  //           </Flex>
-  //           <Flex>
-  //             <Icon as={IconMail} />
-            
-  //           </Flex>
-  //           <Flex>
-  //             <Icon as={IconLock} />
-              
-  //           </Flex>
-  //           <Flex align="end">
-  //             <Icon as={IconEdit} />
-  //             <Text>Edit Information</Text>
-  //           </Flex>
-  //     </VStack>
-      
-  // )
+ 
 
 export default InfoForm; 
