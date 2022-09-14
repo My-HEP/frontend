7d30167adc33130e08d3150b7b994209f26dd3d8
Kristen Coy <kristenwcoy@gmail.com>
2022-09-14 16:17:45 -0400

    Add alert for failed/incorrect password

--- src/authSignIn.js
+++ src/authSignIn.js
@@ -3,12 +3,15 @@ import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 const signIn = async (email, password) => {
   const auth = getAuth();
   try {
-    const user = signInWithEmailAndPassword(auth, email, password);
+    const user = await signInWithEmailAndPassword(auth, email, password);
     return user;
   } catch (error) {
     // const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
+    // const errorMessage = error.message;
+    console.log(error.message);
+    if (error.code === 'auth/wrong-password') {
+      alert('Incorrect password.');
+    }
   }
 };
 
