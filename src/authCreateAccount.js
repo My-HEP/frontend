aa2df9fc71928d8295e7496d9d61350c1e94db1e
Kayla Alewine <kayla.alewine@bah.com>
2022-09-19 16:04:24 -0400

    Send account creation data to server

--- src/authCreateAccount.js
+++ src/authCreateAccount.js
@@ -4,6 +4,7 @@ const createAccount = async (email, password) => {
   const auth = getAuth();
   try {
     const user = await createUserWithEmailAndPassword(auth, email, password);
+
     return user;
   } catch (error) {
     // const errorCode = error.code;
