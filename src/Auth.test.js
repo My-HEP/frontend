a437f4ac1c653c52c9af78c6d8b0b0ee5dbeae90
Kayla Alewine <kayla.alewine@bah.com>
2022-10-03 11:48:58 -0400

    Update tests

---
+++ src/Auth.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { render } from '../src/test-utils';
+// import App from './App';
+import Auth from '../src/shared/pages/Auth';
+
+test('returns a user not found error', async () => {
+    // const email = 'notFound@email.com';
+    // const password = '123456';
+    // expect.assertions(1);
+    //   try {
+    //     await Auth.signInHandler(email, password) 
+    //   } catch (e) {
+    //     expect(e).toMatch('A user with this email address does not exist. Please create an account.');
+    //   }
+});
