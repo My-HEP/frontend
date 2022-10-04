ea90acf0d32728f43e2d4c71bb329a7c0dcaca41
Kayla Alewine <kayla.alewine@bah.com>
2022-10-04 07:46:44 -0400

    Add error state and error message to input fields

--- src/shared/components/CreateAccountModal.js
+++ src/shared/components/CreateAccountModal.js
@@ -9,9 +9,11 @@ import {
   ModalBody,
   ModalCloseButton,
   FormLabel,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
   Input,
   useDisclosure,
-  useToast,
   useColorModeValue,
   Stack,
 } from '@chakra-ui/react';
@@ -22,10 +24,18 @@ import { useNavigate } from 'react-router-dom';
 
 function CreateAccountModal() {
   const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [verifyPassword, setVerifyPassword] = useState('');
+  const [verifyPasswordError, setVerifyPasswordError] = useState('');
   const [firstName, setFirstName] = useState('');
+  const [firstNameError, setFirstNameError] = useState('');
   const [lastName, setLastName] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
+  const [phoneNumberError, setPhoneNumberError] = useState('');
+
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -34,16 +44,41 @@ function CreateAccountModal() {
   const finalRef = React.useRef(null);
 
   const navigate = useNavigate();
-  const toast = useToast();
 
-  const createAccountHandler = async (email, password, firstName, lastName) => {
-    if (!firstName || !email || !password || !lastName) {
-      toast({
-        title: 'Please include all fields.',
-        status: 'error',
-      });
-      return;
+  // Validate fields
+  const validateFormValues = (formValues) => {
+    console.log(formValues)
+    if(email === ""){
+      setEmailError(true)
     }
+    if ( firstName === ""){
+      setFirstNameError(true)
+    }
+    if (lastName === ""){
+      setLastNameError(true)
+    }
+    if (phoneNumber === ""){
+      setPhoneNumberError(true)
+    }
+    if (password === ""){
+      setPasswordError(true)
+      setVerifyPasswordError(true)
+    }
+    if (phoneNumber === ""){
+      setPhoneNumberError(true)
+    } else {
+      if(password !== verifyPassword) {
+        setVerifyPasswordError(true);
+       } 
+    }
+
+  }
+
+
+  const createAccountHandler = async ({...formValues}) => {
+    validateFormValues(formValues)
+   
+    // Save user to database
     let res = await createAccount(email, password);
     const uid = res.user.uid;
     console.log(uid);
@@ -52,7 +87,6 @@ function CreateAccountModal() {
       lastName,
       phoneNumber,
       email,
-      password,
       uid,
     };
     fetch('http://localhost:3001/user', {
@@ -66,6 +100,7 @@ function CreateAccountModal() {
     console.log(res);
     navigate('/home');
   };
+  
 
   return (
     <>
@@ -90,11 +125,12 @@ function CreateAccountModal() {
         onClose={onClose}
       >
         <ModalOverlay />
-        <ModalContent padding="1.5rem">
+        <ModalContent padding="1rem">
           <ModalHeader>Sign up with your email</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
+          <ModalBody pb={4}>
             <FloatingFormControl>
+            <FormControl isInvalid={firstNameError}> 
               <Input
                 ref={initialRef}
                 placeholder=" "
@@ -104,50 +140,78 @@ function CreateAccountModal() {
                 value={firstName}
               />
               <FormLabel backgroundColor={labelColor}>First name</FormLabel>
+              {!firstNameError ? <></> : <FormErrorMessage>First name is required.</FormErrorMessage>}
+            </FormControl>
             </FloatingFormControl>
 
             <FloatingFormControl>
-              <Input
-                placeholder=" "
-                type="text"
-                focusBorderColor="teal.600"
-                onChange={event => setLastName(event.target.value)}
-                value={lastName}
-              />
-              <FormLabel backgroundColor={labelColor}>Last name</FormLabel>
+              <FormControl isInvalid={lastNameError}>
+                <Input
+                  placeholder=" "
+                  type="text"
+                  focusBorderColor="teal.600"
+                  onChange={event => setLastName(event.target.value)}
+                  value={lastName}
+                />
+                <FormLabel backgroundColor={labelColor}>Last name</FormLabel>
+                {!lastNameError ? <></> : <FormErrorMessage>Last name is required.</FormErrorMessage>}
+              </FormControl>
             </FloatingFormControl>
 
             <FloatingFormControl>
-              <Input
-                type="tel"
-                placeholder=" "
-                focusBorderColor="teal.600"
-                onChange={event => setPhoneNumber(event.target.value)}
-                value={phoneNumber}
-              />
-              <FormLabel backgroundColor={labelColor}>Phone number</FormLabel>
+              <FormControl isInvalid={phoneNumberError}>
+                <Input
+                  type="tel"
+                  placeholder=" "
+                  focusBorderColor="teal.600"
+                  onChange={event => setPhoneNumber(event.target.value)}
+                  value={phoneNumber}
+                />
+                <FormLabel backgroundColor={labelColor}>Phone number</FormLabel>
+              {!phoneNumberError ? <></> : <FormErrorMessage>Phone number is required.</FormErrorMessage>}
+              </FormControl>
             </FloatingFormControl>
 
             <FloatingFormControl>
-              <Input
-                type="email"
-                placeholder=" "
-                focusBorderColor="teal.600"
-                onChange={event => setEmail(event.target.value)}
-                value={email}
-              />
-              <FormLabel backgroundColor={labelColor}>Email address</FormLabel>
+              <FormControl isInvalid={emailError}>
+                <Input
+                  type="email"
+                  placeholder=" "
+                  focusBorderColor="teal.600"
+                  onChange={event => setEmail(event.target.value)}
+                  value={email}
+                />
+                <FormLabel backgroundColor={labelColor}>Email address</FormLabel>
+              {!emailError ? <></> : <FormErrorMessage>Email is required.</FormErrorMessage>}
+              </FormControl>
             </FloatingFormControl>
 
             <FloatingFormControl>
-              <Input
-                type="password"
-                placeholder=" "
-                focusBorderColor="teal.600"
-                onChange={event => setPassword(event.target.value)}
-                value={password}
-              />
-              <FormLabel backgroundColor={labelColor}>Password</FormLabel>
+              <FormControl isInvalid={passwordError}>
+                <Input
+                  type="password"
+                  placeholder=" "
+                  focusBorderColor="teal.600"
+                  onChange={event => setPassword(event.target.value)}
+                  value={password}
+                />
+                <FormLabel backgroundColor={labelColor}>Password</FormLabel>
+                {!passwordError ? <FormHelperText>Passwords must be at least 6 characters in length.</FormHelperText>  : <FormErrorMessage>Password of at least 6 characters is required.</FormErrorMessage>}
+              </FormControl>
+            </FloatingFormControl>
+
+            <FloatingFormControl>
+              <FormControl isInvalid={verifyPasswordError}>
+                <Input
+                  type="password"
+                  placeholder=" "
+                  focusBorderColor="teal.600"
+                  onChange={event => setVerifyPassword(event.target.value)}
+                  value={verifyPassword}
+                />
+                <FormLabel backgroundColor={labelColor}>Confirm Password</FormLabel>
+              {!verifyPasswordError ? <FormHelperText></FormHelperText>  : <FormErrorMessage>Passwords must match.</FormErrorMessage>}
+              </FormControl>
             </FloatingFormControl>
           </ModalBody>
 
@@ -161,7 +225,7 @@ function CreateAccountModal() {
                 colorScheme="teal"
                 variant="outline"
                 onClick={() =>
-                  createAccountHandler(email, password, firstName, lastName)
+                  createAccountHandler({email, password, verifyPassword, firstName, lastName})
                 }
               >
                 Create an account
