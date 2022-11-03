669644ffe51d0dd916ca22cb25edd7ecfc6cd977
Kristen Coy <kristenwcoy@gmail.com>
2022-11-03 10:23:46 -0400

    Add waves to top of home page

--- src/therapist/pages/Home.js
+++ src/therapist/pages/Home.js
@@ -9,6 +9,8 @@ import {
   VStack,
   Avatar,
   Box,
+  AspectRatio,
+  Image,
 } from '@chakra-ui/react';
 import Confirmation from '../components/LogoutConfirmation';
 import EditModal from '../../shared/components/Modal';
@@ -16,6 +18,7 @@ import { ColorModeSwitcher } from '../../ColorModeSwitcher';
 import { IconUsers, IconBarbell } from '@tabler/icons';
 import { logoIcon } from '../../shared/components/LogoIcon';
 import { getAuth } from 'firebase/auth';
+import waves from './layered-waves-haikei (2).png';
 
 function TherapistHome() {
   const [userData, setUserData] = useState([]);
@@ -60,39 +63,45 @@ function TherapistHome() {
   {
     if (user) {
       return (
-        <Flex
-          direction="column"
-          justifyContent="center"
-          align="center"
-          height="100vh"
-          padding="0"
-          overflow="scroll"
-        >
+        <>
           <Flex
-            height="100%"
             direction="column"
-            justify="center"
-            align={['center', 'center', 'start']}
-            marginLeft={['10', '10', '20%']}
-            marginRight={['10', '10', '20%']}
-            // marginTop={['20', '20', '40']}
-            paddingTop="5rem"
-            paddingBottom="100px"
-            minWidth="10rem"
+            // justifyContent="center"
+            align="center"
+            height="100vh"
+            padding="0"
+            zIndex="1"
+            overflow="scroll"
           >
-            <VStack align="start" marginLeft={['1rem', '0']}>
-              <Heading size={'3xl'}>Welcome back,</Heading>
-              {userData.firstName ? (
-                <Heading size={'2xl'} textAlign={['center', 'left']}>
-                  {userData.firstName}
-                </Heading>
-              ) : (
-                <Heading size={'2xl'} textAlign={['center', 'left']}>
-                  Therapist
-                </Heading>
-              )}
-            </VStack>
-            {/* <Flex
+            <AspectRatio ratio={16 / 9}>
+              <Image src={waves} position="fixed" />
+            </AspectRatio>
+            <Flex
+              height="100%"
+              direction="column"
+              // justify="center"
+              align={['center', 'center', 'start']}
+              marginLeft={['10', '10', '20%']}
+              marginRight={['10', '10', '20%']}
+              marginTop={['20', '20', '40']}
+              paddingTop="5rem"
+              paddingBottom="100px"
+              minWidth="10rem"
+              zIndex="1"
+            >
+              <VStack align="start" marginLeft={['1rem', '0']}>
+                <Heading size={'3xl'}>Welcome back,</Heading>
+                {userData.firstName ? (
+                  <Heading size={'2xl'} textAlign={['center', 'left']}>
+                    {userData.firstName}
+                  </Heading>
+                ) : (
+                  <Heading size={'2xl'} textAlign={['center', 'left']}>
+                    Therapist
+                  </Heading>
+                )}
+              </VStack>
+              {/* <Flex
             gap={['2', '5']}
             mb={['1rem', '1rem', '2rem']}
             marginLeft={['0', '0', '3rem']}
@@ -101,80 +110,85 @@ function TherapistHome() {
             minWidth={['300px', '300px', '500px']}
           >
             {/* <Icon as={logoIcon} boxSize={['5rem', '8rem']} /> */}
-            {/* </Flex> */}
-            <Flex
-              direction={['column', 'column', 'row']}
-              // margin={['1rem 0', '0.5rem 0', '2rem 0']}
-              marginTop="5rem"
-            >
-              <VStack
-                spacing={[3, 3, 5]}
-                width={['100%', '100%', '50%']}
-                minWidth="300px"
-                align={['center', 'center', 'start']}
-                padding="0px"
+              {/* </Flex> */}
+              <Flex
+                direction={['column', 'column', 'row']}
+                // margin={['1rem 0', '0.5rem 0', '2rem 0']}
+                marginTop="5rem"
               >
-                <Avatar
-                  name={variables.userName}
-                  src={variables.avatar}
-                  size="2xl"
-                />
-
-                <VStack spacing={5} align="start">
-                  <Flex minWidth="200px" gap="3" justify="start" align="start">
-                    <Text as="b" fontSize="2xl">
-                      {variables.userName}
-                    </Text>
-                  </Flex>
-                  <EditModal type={'self'} />
-                  <Confirmation />
+                <VStack
+                  spacing={[3, 3, 5]}
+                  width={['100%', '100%', '50%']}
+                  minWidth="300px"
+                  align={['center', 'center', 'start']}
+                  padding="0px"
+                >
+                  <Avatar
+                    name={variables.userName}
+                    src={variables.avatar}
+                    size="2xl"
+                  />
+                  <VStack spacing={5} align="start">
+                    <Flex
+                      minWidth="200px"
+                      gap="3"
+                      justify="start"
+                      align="start"
+                    >
+                      <Text as="b" fontSize="2xl">
+                        {variables.userName}
+                      </Text>
+                    </Flex>
+                    <EditModal type={'self'} />
+                    <Confirmation />
+                  </VStack>
                 </VStack>
-              </VStack>
-              <VStack
-                spacing={[6, 7, 10]}
-                width={['100%', '100%', '50%']}
-                mt={['3rem', '3rem', 0]}
-                minWidth="300px"
+                <VStack
+                  spacing={[6, 7, 10]}
+                  width={['100%', '100%', '50%']}
+                  mt={['3rem', '3rem', 0]}
+                  minWidth="300px"
+                >
+                  <Text as="b">
+                    Currently serving {variables.patientNum} patients
+                  </Text>
+                  <Link to="/patients">
+                    <Button
+                      leftIcon={<IconUsers />}
+                      colorScheme="teal"
+                      variant="outline"
+                      size="lg"
+                      minWidth="14.5rem"
+                    >
+                      My Patients
+                    </Button>
+                  </Link>
+                  <Text as="b">{variables.exerciseNum} Exercises uploaded</Text>
+                  <Link to="/exerciselibrary">
+                    <Button
+                      leftIcon={<IconBarbell />}
+                      colorScheme="teal"
+                      variant="outline"
+                      size="lg"
+                      minWidth="14.5rem"
+                    >
+                      Exercise Library
+                    </Button>
+                  </Link>
+                </VStack>
+              </Flex>
+              <Box
+                display="block"
+                position="fixed"
+                bottom={['10px', '10px', '30px']}
+                align="center"
+                left={['4px', '7px', '20px']}
               >
-                <Text as="b">
-                  Currently serving {variables.patientNum} patients
-                </Text>
-                <Link to="/patients">
-                  <Button
-                    leftIcon={<IconUsers />}
-                    colorScheme="teal"
-                    variant="outline"
-                    size="lg"
-                    minWidth="14.5rem"
-                  >
-                    My Patients
-                  </Button>
-                </Link>
-                <Text as="b">{variables.exerciseNum} Exercises uploaded</Text>
-                <Link to="/exerciselibrary">
-                  <Button
-                    leftIcon={<IconBarbell />}
-                    colorScheme="teal"
-                    variant="outline"
-                    size="lg"
-                    minWidth="14.5rem"
-                  >
-                    Exercise Library
-                  </Button>
-                </Link>
-              </VStack>
+                <ColorModeSwitcher />
+              </Box>
             </Flex>
-            <Box
-              display="block"
-              position="fixed"
-              bottom={['10px', '10px', '30px']}
-              align="center"
-              left={['4px', '7px', '20px']}
-            >
-              <ColorModeSwitcher />
-            </Box>
           </Flex>
-        </Flex>
+        </>
       );
     } else {
       return (
