4e037fc93687e495a6e0019164ce6d3031d77717
Kayla Alewine <kayla.alewine@bah.com>
2022-12-02 10:21:43 -0500

    Remove routes from app.js and remove unncessessary code from protected routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import { useFirebaseAuth } from './context/FirebaseAuthContext';
-import { useEffect, useState } from 'react';
-
 import { ChakraProvider, theme } from '@chakra-ui/react';
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
-
 import { firebaseConfig } from '../src/utils/firebase';
-import { FirebaseAuthProvider } from './context/FirebaseAuthContext';
-
-import { Routes, Route } from 'react-router-dom';
-import Auth from './shared/pages/Auth';
-import TherapistHome from './therapist/pages/Home';
-import Patients from './therapist/pages/Patients';
-import HEP from './therapist/pages/HEP';
-import ExerciseLibrary from './therapist/pages/ExerciseLibrary';
 
 function App() {
   initializeApp(firebaseConfig);
 
   return (
     <ChakraProvider theme={theme}>
-      <Routes>
-        <Outlet />
-      </Routes>
+      <Outlet />
     </ChakraProvider>
   );
 }
--- src/ProtectedRoutes.js
+++ src/ProtectedRoutes.js
@@ -1,21 +1,12 @@
-import { useState } from 'react';
+import React from 'react';
 import {Navigate, Outlet, useLocation} from 'react-router-dom';
 import { useFirebaseAuth } from './context/FirebaseAuthContext';
 
 
 const ProtectedRoutes=({ allowedRoles }) => {
-    const { auth } = useFirebaseAuth() ?? {};
+    const { auth, userRole } = useFirebaseAuth() ?? {};
     const location = useLocation();
-    const uid = auth?.currentUser?.uid;
-    const [userRole, setUserRole] = useState('PATIENT');
-    
 
-    const fetchUserRole = async (req, res) => {
-       const response = await fetch(`http://localhost:3001/user/${uid}`);
-       const userResponse = await response.json();
-       setUserRole(userResponse.role);
-   };
-    fetchUserRole();
     return auth && userRole === allowedRoles[0] ? <Outlet /> : <Navigate to='/' state={{from: location}} replace />
 }
 
